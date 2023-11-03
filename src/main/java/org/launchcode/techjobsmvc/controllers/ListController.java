e990d725527f03cb712952158946f0353e6abaec
Taylor016 <taylorr.016@gmail.com>
2023-11-02 21:25:20 -0500

    updates

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -28,7 +28,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
-        tableChoices.put("all", "https://java-techjobs-mvc.launchcodetechnicaltraining.org/list/jobs?column=all");
+        tableChoices.put("all","all");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
